2db798b9879ee765bf839c2f21189bf3116908dd
andrasputyera <andras.putyera@gmail.com>
2021-03-19 13:21:04 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>How fast can you react?</h1>
+  <button @click="start" :disabled="isPlaying" >Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score"/>
+  <!-- <p v-if="showResults">Your reaction time is {{ score }} ms</p> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from '@/components/Block.vue';
+import Results from '@/components/Results.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -23,4 +45,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
